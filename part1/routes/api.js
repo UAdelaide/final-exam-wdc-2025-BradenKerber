1b4886d79d9dd289ad93f3ee96631185ecdc2024
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 03:12:30 +0000

    [automatic commit] 2025-06-20 03:12:30 UTC 180.150.112.134

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+/*  */
 router.get('/dogs', function(req, res) {
     req.pool.getConnection(function(err, connection) {
         if (err) {

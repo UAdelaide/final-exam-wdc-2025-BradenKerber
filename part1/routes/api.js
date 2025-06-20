a065b0357d21393644c72f5de5ad90cde1578ec4
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 03:46:38 +0000

    [automatic commit] 2025-06-20 03:46:38 UTC 180.150.112.134

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -3,22 +3,25 @@ var router = express.Router();
 
 /* In file api.js routed through /api */
 router.get('/dogs', function(req, res) {
-    req.pool.getConnection(function(err, connection) {
-        if (err) {
-            res.status(500).send("Could not connect to database.");
-            return;
-        }
+    try {
+        req.pool.getConnection(function(err, connection) {
+            if (err) {
+                res.status(500).send("Could not connect to database.");
+                return;
+            }
 
-        var query = "SELECT name AS dog_name, size, username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id = User.user_id";
-        try {
+            var query = "SELECT name AS dog_name, size, username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id";
             connection.query(query, function(error, rows) {
                 connection.release();
-                res.status(200).json(rows);
+                if (error) {
+                    res.status(500).send("Invalid query.");
+                } else {
+                    res.status(200).json(rows);
+                }
             });
-        } catch (error) {
-            res.status(500).send("Invalid query.");
-        }
-    });
+        });
+    }
+
 });
 
 module.exports = router;

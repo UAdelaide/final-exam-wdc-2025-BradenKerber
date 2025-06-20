fc8a954ff985c8cbd08a852eb3aa4be92493a86c
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 12:01:45 +0000

    [automatic commit] 2025-06-20 12:01:45 UTC 180.150.112.134

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
     if (rows.role == 'owner') {
-      res.redirect
+      res.redirect('/owner-dashboard')
     }
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {

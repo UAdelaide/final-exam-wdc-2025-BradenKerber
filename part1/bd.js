f40ad624f9424d79fe0d8ecfb7ff81b56bbf346f
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 02:02:54 +0000

    [automatic commit] 2025-06-20 02:02:54 UTC 180.150.112.134

--- part1/bd.js
+++ part1/bd.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 // create a 'pool' (group) of connections to be used for connecting with our SQL server
 var dbConnectionPool = mysql.createPool({
     host: 'localhost',
-    database: ''
+    database: 'DogWalkService'
 });
 
 module.exports = dbConnectionPool;

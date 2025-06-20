47d961e433fa82e26c850ce8587d5431836eb82e
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 12:52:39 +0000

    [automatic commit] 2025-06-20 12:52:39 UTC 180.150.112.134

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -51,7 +51,6 @@ router.post('/login', async (req, res) => {
 
     req.session.user = { id: rows[0].user_id,
       username:rows[0].username,
-      role:rows[0].role
     };
 
     res.json({ message: 'Login successful', user: rows[0] });

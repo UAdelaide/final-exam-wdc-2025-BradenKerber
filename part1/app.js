32af46092ecb4d0cd2f0f52c8cab0f79f051b2de
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 10:11:18 +0000

    [automatic commit] 2025-06-20 10:11:18 UTC 180.150.112.134

--- part1/app.js
+++ part1/app.js
@@ -39,7 +39,7 @@ let db;
                 INSERT INTO Users (username, email, password_hash, role) VALUES
                 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
                 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-                ('carol123', 'carol@example.com', 'hashed789', 'owner')
+                ('carol123', 'carol@example.com', 'hashed789', 'owner'),
                 ('magnum', 'magnumpi@example.com', 'hashbrown', 'walker');
             `);
         }

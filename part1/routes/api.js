c834dd4a6d776c95b415a58623050a18f163df1d
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 03:49:19 +0000

    [automatic commit] 2025-06-20 03:49:19 UTC 180.150.112.134

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* In file api.js routed through /api */
-router.get('/dogs', function(req, res) {
+router.get('/dogs', async function(req, res) {
     try {
+        const [rows] = await db.query(`
+    SELECT bl.BookID, bi.Title, u.Name AS SellerName, bl.SellerID
+    FROM BookListings bl
+    JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoID
+    JOIN Users u ON bl.SellerID = u.UserID
+  `);
         req.pool.getConnection(function(err, connection) {
             var query = "SELECT name AS dog_name, size, username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id";
             connection.query(query, function(error, rows) {

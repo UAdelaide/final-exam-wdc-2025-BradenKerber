0aaa6cd06d7475dfe6a8b6f85d92ab52bb60720d
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 10:14:38 +0000

    [automatic commit] 2025-06-20 10:14:38 UTC 180.150.112.134

--- part1/app.js
+++ part1/app.js
@@ -48,8 +48,8 @@ let db;
         if (rows[0].count === 0) {
             await db.execute(`
                 INSERT INTO Dogs (owner_id, name, size) VALUES
-                ('1', 'Max', 'medium'),
-                ('3', 'Bella', 'small');
+                (1, 'Max', 'medium'),
+                (3, 'Bella', 'small');
             `);
         }
 
@@ -57,8 +57,8 @@ let db;
         if (rows[0].count === 0) {
         await db.execute(`
             INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location) VALUES
-            ('1', '2025-06-10 08:00:00', '30', 'Parklands'),
-            ('2', '2025-06-10 09:30:00', '45', 'Beachside Ave');
+            (1, '2025-06-10 08:00:00', 30, 'Parklands'),
+            (2, '2025-06-10 09:30:00', 45, 'Beachside Ave');
         `);
         }
 

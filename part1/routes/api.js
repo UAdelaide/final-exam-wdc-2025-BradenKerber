177e68220556e0aa35f57d19e6b82cf1bb21e758
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 09:36:07 +0000

    [automatic commit] 2025-06-20 09:36:07 UTC 180.150.112.134

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -26,8 +26,7 @@ router.get('/walkrequests/open', async (req, res) => {
 
 router.get('/walkers/summary', async (req, res) => {
     try {
-        const [rows] = await database.query(` AS average_rating, count() AS completed_walks
-            FROM WalkRatings INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE status = "Open"`);
+        const [rows] = await database.query(`SELECT Users.username, COUNT(WalkRatings.rating) AS total_ratings, ROUND(AVG(rating), 1) AS average, COUNT(WalkRatings.rating_id) AS completed_walks FROM WalkRatings INNER JOIN Users ON WalkRatings.walker_id = Users.user_id GROUP BY Users.username;`);
         res.json(rows);
     } catch (err) {
         res.status(500).send("Server error.");

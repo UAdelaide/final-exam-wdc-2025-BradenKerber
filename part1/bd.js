289aeccb2f5d55eac247aed9c447541c762f069a
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 02:02:13 +0000

    [automatic commit] 2025-06-20 02:02:13 UTC 180.150.112.134

--- part1/bd.js
+++ part1/bd.js
@@ -3,6 +3,8 @@ var mysql = require('mysql');
 
 // create a 'pool' (group) of connections to be used for connecting with our SQL server
 var dbConnectionPool = mysql.createPool({
- host: 'localhost',
-	database: '
+    host: 'localhost',
+    database: ''
 });
+
+module.exports

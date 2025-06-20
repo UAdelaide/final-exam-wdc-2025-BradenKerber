e61febb678a74145d1e6ddaf8ae959559fec35cf
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 03:30:35 +0000

    [automatic commit] 2025-06-20 03:30:35 UTC 180.150.112.134

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* In file api.js routed through /api */
 router.get('/dogs', function(req, res) {
-    try {
     req.pool.getConnection(function(err, connection) {
         if (err) {
             res.sendStatus(500).send("Could not connect to database.");
@@ -19,9 +18,6 @@ router.get('/dogs', function(req, res) {
             res.json(rows);
         });
     });
-    } catch (err) {
-        res.sendStatus(500).send("Could not connect to database.");
-    }
 });
 
 module.exports = router;

4e1cd481e55558a21a5293a7ff6d3d5afbddaf07
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 03:57:41 +0000

    [automatic commit] 2025-06-20 03:57:41 UTC 180.150.112.134

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -21,4 +21,23 @@ router.get('/dogs', function(req, res) {
     });
 });
 
+router.get('/walkrequests/open', function(req, res) {
+    req.pool.getConnection(function(err, connection) {
+        if (err) {
+            res.status(500).send("Could not connect to database.");
+            return;
+        }
+
+        var query = "SELECT name AS dog_name, size, username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id = User.user_id";
+        connection.query(query, function(error, rows) {
+            connection.release();
+            if (error) {
+                res.status(500).send("Invalid query.");
+            } else {
+                res.status(200).json(rows);
+            }
+        });
+    });
+});
+
 module.exports = router;

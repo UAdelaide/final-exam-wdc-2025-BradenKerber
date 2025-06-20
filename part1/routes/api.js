aae0e4ead5075d18048546fdd891280a4f9e9b82
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 03:56:21 +0000

    [automatic commit] 2025-06-20 03:56:21 UTC 180.150.112.134

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* In file api.js routed through /api */
-router.get('/dogs', async function(req, res) {
-    const [rows] = await.req.pool
+router.get('/dogs', function(req, res) {
     req.pool.getConnection(function(err, connection) {
         if (err) {
             res.status(500).send("Could not connect to database.");

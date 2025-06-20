5c72bac300b2b3d15ddf6a9557b0c3d84a4c9beb
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 03:51:00 +0000

    [automatic commit] 2025-06-20 03:51:00 UTC 180.150.112.134

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,24 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* In file api.js routed through /api */
-router.get('/dogs', async function(req, res) {
-    try {
-        const [rows] = await dbConnectionPool.query(`SELECT name AS dog_name, size, username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id`);
-        req.pool.getConnection(function(err, connection) {
-            var query = "SELECT name AS dog_name, size, username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id";
-            connection.query(query, function(error, rows) {
-                connection.release();
-                if (error) {
-                    res.status(500).send("Invalid query.");
-                } else {
-                    res.status(200).json(rows);
-                }
-            });
-        });
-    } catch (err) {
-        res.status(500).send("Could not connect to database.");
-    }
+router.get('/dogs', function(req, res) {
+    req.pool.getConnection(function(err, connection) {
+        if (err) {
+            res.status(500).send("Could not connect to database.");
+            return;
+        }
 
+        var query = "SELECT name AS dog_name, size, username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id = User.user_id";
+        connection.query(query, function(error, rows) {
+            connection.release();
+            if (error) {
+                res.status(500).send("Invalid query.");
+            } else {
+                res.status(200).json(rows);
+            }
+        });
+    });
 });
 
 module.exports = router;

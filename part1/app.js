3ec705eb433cb71f2e53e20d53af989aa9634716
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 02:04:34 +0000

    [automatic commit] 2025-06-20 02:04:34 UTC 180.150.112.134

--- part1/app.js
+++ part1/app.js
@@ -6,12 +6,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var mysql = require('mysql');
-
-var dbConnectionPool = mysql.createPool({
-    host: 'localhost',
-    database: 'DogWalkService'
-});
 
 module.exports = dbConnectionPool;
 

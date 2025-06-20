afde675171e65f5df8bace5314cd6be1d54cd2bf
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 03:24:13 +0000

    [automatic commit] 2025-06-20 03:24:13 UTC 180.150.112.134

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -11,7 +11,8 @@ router.get('/dogs', function(req, res) {
 
         var query = "SELECT name AS dog_name, size, username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id = User.user_id";
 
-        try {connection.query(query, function(error, rows, fields) {
+        try {
+            connection.query(query, function(error, rows, fields) {
                 connection.release();
                 res.json(rows);
             });

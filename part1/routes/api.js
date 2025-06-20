89549dc571516d5c60d21dba195e1602904fa868
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-20 03:44:58 +0000

    [automatic commit] 2025-06-20 03:44:58 UTC 180.150.112.134

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -11,16 +11,18 @@ router.get('/dogs', function(req, res) {
 
         var query = "SELECT name AS dog_name, size, username AS owner_username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id";
         try {
-            
+            connection.query(query, function(error, rows) {
+                connection.release();
+                if (error) {
+
+                } else {
+                    res.status(200).json(rows);
+                }
+            });
+        } catch (error) {
+                    res.status(500).send("Invalid query.");
         }
-        connection.query(query, function(error, rows) {
-            connection.release();
-            if (error) {
-                res.status(500).send("Invalid query.");
-            } else {
-                res.status(200).json(rows);
-            }
-        });
+
     });
 });
 

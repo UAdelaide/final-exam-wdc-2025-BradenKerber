55160f20abc23309d4033d0a7e55ab8fa3cae84c
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-21 01:24:22 +0000

    [automatic commit] 2025-06-21 01:24:22 UTC 180.150.112.151

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -66,6 +66,7 @@ router.get('/logout', (req, res) => {
     if (err) {
       res.status(400).send("Error logging out");
     }
+    // Clears cookies and sends 
     res.clearCookie('connect.sid');
     res.status(200).send("logout successful");
     });

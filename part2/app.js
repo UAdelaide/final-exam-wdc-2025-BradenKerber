6e4c44da3dee89f71d03cb7438ead95aa90895f3
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-21 06:33:05 +0000

    [automatic commit] 2025-06-21 06:33:05 UTC 180.150.112.151

--- part2/app.js
+++ part2/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 
-// Sessions middleware
+// Sessions middleware added to handle express sessions
 const session = require('express-session');
 app.use(session({
   secret: 'super-ultra-secret-key',

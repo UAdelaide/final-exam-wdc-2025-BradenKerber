23dff1eda082fd01414d9c802ea9f51274239ba7
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-21 03:13:10 +0000

    [automatic commit] 2025-06-21 03:13:10 UTC 180.150.112.151

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -72,9 +72,11 @@ router.get('/logout', (req, res) => {
     });
 });
 
+// Dogs owned by the user
 router.get('/ownedDogs', async (req, res) => {
     const userid = req.session.user.id;
     try {
+        // Query the database for dogs owned by the user
         const [rows] = await database.query(`SELECT name AS dog_name FROM Dogs
           INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE user_id = ?`, [userid]);
         res.json(rows);

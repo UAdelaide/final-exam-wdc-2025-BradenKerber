17e5605dec10bc840dbb63e1834cba1cdc274e28
Braden <braden.kerber@student.adelaide.edu.au>
2025-06-21 04:48:14 +0000

    [automatic commit] 2025-06-21 04:48:14 UTC 180.150.112.151

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -93,8 +93,8 @@ router.get('/dogs', async (req, res) => {
     try {
         const [rows] = await db.query(`SELECT dog_id, name AS dog_name, size, user_id AS owner_id
           FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id`);
-          const response = await fetch('https://dog.ceo/api/breeds/image/random');
-                                  const data = await res.json();
+          const picResponse = await fetch('https://dog.ceo/api/breeds/image/random');
+          const pictures = await picResponse.json();
         res.json(rows);
     } catch (err) {
         res.status(500).send("Server error.");
